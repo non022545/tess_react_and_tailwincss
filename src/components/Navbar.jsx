74111b1b62dfcc1ce5a2c3436d6fbd9960550416
non022545 <nontthont7@gmail.com>
2025-04-10 17:26:30 +0700

    fsdfsd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <nav className="fixed top-0 left-0 w-full gap-3 bg-black text-white p-4 flex justify-end">
+        <nav className="fixed top-0 left-0 w-full gap-3 bg-slate-800 text-white p-4 flex justify-end">
                 <Link to="/">Home</Link>
                 <Link to="/Profile">Profile</Link>
                 <Link to="/Contact">Contact</Link>
